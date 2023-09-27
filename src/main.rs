5c838e1b0d7796e4b3716712234ef555aa3e1020
Sam Rose <hello@samwho.dev>
2023-09-27 10:35:25 +0100

    Move away from termion, it doesn't seem to work on Windows.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use anyhow::Error;
 use log::debug;
+use owo_colors::{OwoColorize, Rgb};
 use std::cmp::max;
 use std::fs::File;
-use std::io::Read;
-use std::io::{stdout, Write};
+use std::io::{stdout, Read, Write};
 use std::path::PathBuf;
 use std::result::Result;
 use structopt::StructOpt;
-use termion::color;
-use termion::terminal_size;
+use terminal_size::terminal_size;
 
 #[derive(StructOpt)]
 struct Cli {
@@ -32,8 +31,8 @@ fn main() -> Result<(), Error> {
 
     debug!("file: {}, size: {}", args.file.display(), file_size);
 
-    let mut grid_width;
-    let mut grid_height;
+    let mut grid_width = 80;
+    let mut grid_height = 24;
 
     match (args.width, args.height) {
         (Some(width), Some(height)) => {
@@ -49,9 +48,10 @@ fn main() -> Result<(), Error> {
             grid_height = height;
         }
         _ => {
-            let (term_width, term_height) = terminal_size()?;
-            grid_width = max(1, term_width as u64 / 2);
-            grid_height = max(1, (term_height as f64 / 1.1).ceil() as u64);
+            if let Some((term_width, term_height)) = terminal_size() {
+                grid_width = max(1, term_width.0 as u64 / 2);
+                grid_height = max(1, (term_height.0 as f64 / 1.1).ceil() as u64);
+            }
 
             if let Some(width) = args.width {
                 grid_width = width;
@@ -94,14 +94,8 @@ fn main() -> Result<(), Error> {
             if count == chunk_size {
                 let avg = sum / count;
                 let intensity = 255 - avg as u8;
-                let c = color::Rgb(intensity, intensity, intensity);
-                write!(stdout, "{}{}  ", color::Fg(c), color::Bg(c))?;
-                write!(
-                    stdout,
-                    "{}{}",
-                    color::Fg(color::Reset),
-                    color::Bg(color::Reset)
-                )?;
+                let c = Rgb(intensity, intensity, intensity);
+                write!(stdout, "{}", "  ".on_color(c))?;
                 sum = 0;
                 count = 0;
                 total_chunks += 1;
